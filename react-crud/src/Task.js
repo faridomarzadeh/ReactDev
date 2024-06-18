fab13a6101b8d832743a04b611257171d13ce59e
Farid Omarzadeh <farid.omarzadeh29@gmail.com>
2024-06-18 13:50:42 -0500

    styling

--- react-crud/src/Task.js
+++ react-crud/src/Task.js
@@ -4,8 +4,8 @@ export const Task = (props) => {
   return (
     <div className="show-task__item" key={props.task.id}>
       <h1 style={{color: props.task.completed?"Green":"Black"}}>{props.task.taskName}</h1>
-      <button onClick={() => props.completeTask(props.task)}>Complete</button>
-      <button onClick={() => props.deleteTask(props.task.id)}>X</button>
+      {props.task.completed? <span>&#10003;</span>: <button onClick={() => props.completeTask(props.task)}>Complete</button>}
+      <button onClick={() => props.deleteTask(props.task.id)}>Delete</button>
     </div>
   );
 };
