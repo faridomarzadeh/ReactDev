4211f76caf0c51be8224196cd63f6512cbae1396
Farid Omarzadeh <farid.omarzadeh29@gmail.com>
2025-10-18 13:51:34 -0400

    Js Review finished

--- Ultimate-react-course/02-JS-REVIEW/script.js
+++ Ultimate-react-course/02-JS-REVIEW/script.js
@@ -66,7 +66,7 @@ const data = [
     publicationDate: "1965-01-01",
     author: "Frank Herbert",
     genres: ["science fiction", "novel", "adventure"],
-    hasMovieAdaptation: true,
+    hasMovieAdaptation: false,
     pages: 658,
     translations: {
       spanish: "",
@@ -143,6 +143,7 @@ function getBook(id) {
   return data.find((d) => d.id === id);
 }
 
+/*
 const book = getBook(2);
 const { title, author, pages, publicationDate, genres, hasMovieAdaptation } =
   book;
@@ -154,7 +155,122 @@ const newGenres = ["epic fantasy", ...genres];
 newGenres;
 const updatedBook = {
   ...book,
+  //adding a new property
   moviePublicationDate: "2005-10-13",
+
+  //overwriting an existing property
   pages: 1210,
 };
 updatedBook;
+
+const summary = `${title}, a ${pages}-page long book, was written by ${author} and published in ${
+  publicationDate.split("-")[0]
+}. The book has ${hasMovieAdaptation ? "" : "not"} been adapted as a movie`;
+summary;
+
+const pageRange = pages > 1000 ? "over a thousand" : "less than 1000";
+pageRange;
+
+const getYear = (str) => str.split("-")[0];
+
+console.log(getYear(publicationDate));
+
+console.log("Hello" && "The second value is being shown");
+console.log(0 && "the value won't be shown");
+
+const spanishTranslation = book.translations.spanish || "No Translation";
+
+spanishTranslation;
+
+//nullish coalescing
+console.log(0 ?? 18);
+console.log("" ?? 18);
+console.log(false ?? 18);
+//only these two values are effective falsy values for nullish coalescing
+console.log(undefined ?? 18);
+console.log(null ?? 18);
+*/
+
+/*
+function getTotalReviewCount(book) {
+  const goodreads = book.reviews?.goodreads?.reviewsCount ?? 0;
+  const librarything = book.reviews?.librarything?.reviewsCount ?? 0;
+
+  return goodreads + librarything;
+}
+
+const books = getBooks();
+books;
+
+const x = [1, 2, 3, 4, 5, 6].map((el) => el * 2);
+console.log(x);
+
+const titles = books.map((book) => book.title);
+titles;
+
+const essentialData = books.map((book) => ({
+  title: book.title,
+  author: book.author,
+  reviewsCount: getTotalReviewCount(book),
+}));
+essentialData;
+
+const longBooksWithMovieAdaptation = books
+  .filter((book) => book.pages > 500)
+  .filter((book) => book.hasMovieAdaptation);
+longBooksWithMovieAdaptation;
+
+const adventureBooks = books
+  .filter((book) => book.genres.includes("adventure"))
+  .map((book) => book.title);
+adventureBooks;
+
+const allBooksPages = books.reduce((sum, book) => sum + book.pages, 0);
+allBooksPages;
+
+const longBooks = books.reduce((longItems, book) => {
+  if (book.pages > 500) longItems.push(book);
+  return longItems;
+}, []);
+longBooks;
+
+const arr = [3, 7, 1, 9, 6];
+const sorted = arr.slice().sort((a, b) => a - b);
+sorted;
+arr;
+
+const sortedByPagesDesc = books.slice().sort((a, b) => b.pages - a.pages);
+sortedByPagesDesc;
+
+const newBook = {
+  id: 6,
+  title: "Harry Potter and the Chamber of Secrets",
+  author: "J. K. Rowling",
+};
+
+const booksAfterAdd = [...books, newBook];
+booksAfterAdd;
+
+const booksAfterDelete = booksAfterAdd.filter((book) => book.id !== 3);
+booksAfterDelete;
+
+const booksAfterUpdate = booksAfterDelete.map((book) =>
+  book.id == 1 ? {...book, pages:1210} : book
+);
+booksAfterUpdate;
+*/
+
+// fetch("https://jsonplaceholder.typicode.com/todos")
+//   .then((response) => response.json())
+//   .then((json) => console.log(json));
+// console.log(fetch("https://jsonplaceholder.typicode.com/todos"));
+
+// console.log("farid");
+
+async function getTodos() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/todos");
+  return await response.json();
+}
+
+getTodos().then((data) => console.log(data));
+console.log("farid");
