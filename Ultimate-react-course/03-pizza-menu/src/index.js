d4c6b9672bea33a3eb23ba193a8319763e8bcded
Farid Omarzadeh <farid.omarzadeh29@gmail.com>
2025-10-19 17:24:59 -0400

    Started the Pizza project

---
+++ Ultimate-react-course/03-pizza-menu/src/index.js
@@ -0,0 +1,22 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+function App() {
+  return (
+    <>
+      <h1>Hello React!</h1>
+      <Pizza />
+    </>
+  );
+}
+
+function Pizza() {
+  return <h2>Pizza</h2>;
+}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
