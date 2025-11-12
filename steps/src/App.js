21b8240daa2f4607bce63e0cdf9bbc13fd0c4409
Farid Omarzadeh <farid.omarzadeh29@gmail.com>
2025-11-11 20:28:03 -0500

    Added state to steps

--- steps/src/App.js
+++ steps/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const messages = [
   "Learn React ⚛️",
   "Apply for jobs 💼",
@@ -5,21 +7,41 @@ const messages = [
 ];
 
 function App() {
-  const step = 3;
+  const [step, setStep] = useState(1);
+  // const [user, setUser] = useState({ name: "John" });
+
+  const maxStepCount = messages.length;
+
+  function handleNext() {
+    if (step < maxStepCount) setStep((prevStep) => prevStep + 1);
+    // setUser((preUser) => ({ ...preUser, name: "josh" }));
+  }
+
+  function handlePrevious() {
+    if (step > 1) setStep((prevStep) => prevStep - 1);
+  }
   return (
     <div className="steps">
       <div className="numbers">
-        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
-        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
-        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+        <div className={step >= 1 ? "active" : ""}>1</div>
+        <div className={step >= 2 ? "active" : ""}>2</div>
+        <div className={step >= 3 ? "active" : ""}>3</div>
       </div>
-      <p className="message">{messages[step - 1]}</p>
+      <p className="message">
+        {messages[step - 1]} {user.name}
+      </p>
       <div className="buttons">
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handlePrevious}
+        >
           Previous
         </button>
 
-        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+        <button
+          style={{ backgroundColor: "#7950f2", color: "#fff" }}
+          onClick={handleNext}
+        >
           Next
         </button>
       </div>
