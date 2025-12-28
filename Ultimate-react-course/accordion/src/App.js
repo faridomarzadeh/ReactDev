56b03a3c4c10396c774504e5069c412a965d0766
Farid Omarzadeh <farid.omarzadeh29@gmail.com>
2025-12-28 08:46:30 -0500

    Added the accordion exercise

---
+++ Ultimate-react-course/accordion/src/App.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import "./styles.css";
+
+const faqs = [
+  {
+    title: "Where are these chairs assembled?",
+    text: "Lorem ipsum dolor sit amet consectetur, adipisicing elit. Accusantium, quaerat temporibus quas dolore provident nisi ut aliquid ratione beatae sequi aspernatur veniam repellendus.",
+  },
+  {
+    title: "How long do I have to return my chair?",
+    text: "Pariatur recusandae dignissimos fuga voluptas unde optio nesciunt commodi beatae, explicabo natus.",
+  },
+  {
+    title: "Do you ship to countries outside the EU?",
+    text: "Excepturi velit laborum, perspiciatis nemo perferendis reiciendis aliquam possimus dolor sed! Dolore laborum ducimus veritatis facere molestias!",
+  },
+];
+
+export default function App() {
+  return (
+    <div className="accordion">
+      <Accordion />
+    </div>
+  );
+}
+
+function Accordion() {
+  return (
+    <div className="accordion">
+      {faqs.map((item, index) => (
+        <AccordionItem
+          title={item.title}
+          text={item.text}
+          rowNumber={index}
+          key={item.title}
+        />
+      ))}
+    </div>
+  );
+}
+
+function AccordionItem({ title, text, rowNumber }) {
+  const [isOpen, setIsOpen] = useState(false);
+  return (
+    <div
+      className={`item ${isOpen ? "open" : ""}`}
+      onClick={() => setIsOpen((isOpen) => !isOpen)}
+    >
+      <p className="number">
+        {rowNumber <= 9 ? `0${rowNumber + 1}` : `${rowNumber + 1}`}
+      </p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen && <div className="content-box">{text}</div>}
+    </div>
+  );
+}
